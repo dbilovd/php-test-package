7c4195fdaa66ec82d39ffbb4b9c55569ca070392
David Lartey <davidlartey237f@gmail.com>
2019-09-24 12:24:09 +0000

    Apply fixes from StyleCI

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -2,27 +2,24 @@
 
 namespace Dbilovd\PHPTestPackage;
 
-use Dbilovd\PHPTestPackage\Jokes\JokeContract;
 use Dbilovd\PHPTestPackage\Jokes\ChuckNorrisJokes;
 
 class JokeFactory
 {
-	/**
-	 * Constructor
-	 *
-	 * @return \Jokes\JokeContract
-	 */
-	public function __construct()
-	{
-	}
+    /**
+     * Constructor.
+     *
+     * @return \Jokes\JokeContract
+     */
+    public function __construct()
+    {
+    }
 
-	/**
-	 * 		
-	 *
-	 * @return [type] [description]
-	 */
-	public function makeJoke()
-	{
-		return new ChuckNorrisJokes();	
-	}
+    /**
+     * @return [type] [description]
+     */
+    public function makeJoke()
+    {
+        return new ChuckNorrisJokes();
+    }
 }
