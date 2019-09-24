fbcbea79aec18713735a0b43806ca9ae1e5462fd
David Lartey <davidlartey237f@gmail.com>
2019-09-24 12:24:17 +0000

    Apply fixes from StyleCI (#1)

--- tests/JokesFactoryTest.php
+++ tests/JokesFactoryTest.php
@@ -2,23 +2,23 @@
 
 namespace Dbilovd\PHPTestPackage\Tests;
 
+use PHPUnit\Framework\TestCase;
 use Dbilovd\PHPTestPackage\JokeFactory;
 use Dbilovd\PHPTestPackage\Jokes\JokeContract;
-use PHPUnit\Framework\TestCase;
 
 class JokesFactoryTest extends TestCase
 {
-	/** @test */
-	public function it_returns_an_instance_of_a_joke_class()
-	{
-		$joke = (new JokeFactory())->makeJoke();
-		$this->assertTrue($joke instanceof JokeContract);
-	}
+    /** @test */
+    public function it_returns_an_instance_of_a_joke_class()
+    {
+        $joke = (new JokeFactory())->makeJoke();
+        $this->assertTrue($joke instanceof JokeContract);
+    }
 
-	/** @test */
-	public function it_returns_a_random_joke()
-	{
-		$joke = (new JokeFactory())->makeJoke();
-		$this->assertTrue(is_string($joke->joke()));
-	}
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $joke = (new JokeFactory())->makeJoke();
+        $this->assertTrue(is_string($joke->joke()));
+    }
 }
